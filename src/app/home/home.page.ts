6fdbcabfd61e9d93ef12800af57b1186cc858f6e
kidalikevin <kidali.a.kevin@gmail.com>
2019-06-24 21:50:22 +0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
+import {Base64} from '@ionic-native/base64/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    myPhoto = '';
+
+    constructor(
+        private base64: Base64,
+        private camera: Camera
+    ) {
+    }
+
+    snapshot() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            mediaType: this.camera.MediaType.PICTURE
+        };
+
+        this.camera.getPicture(options).then((imageData) => {
+            this.myPhoto = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+        });
+    }
 
 }
